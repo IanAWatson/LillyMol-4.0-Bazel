8349ee4a1dd22d6c4c1e2ecb84919ad030c94704
Ian Watson <ianiwatson@gmail.com>
2022-04-15 12:11:44 -0400

    WIP leveldb_list

---
+++ src/Utilities/General/leveldb_list.py
@@ -0,0 +1,28 @@
+# List the contents of a leveldb database
+
+from absl import app
+from absl import flags
+from absl import logging
+
+import plyvel
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_boolean('verbose', False, 'Verbose output')
+
+def main(argv):
+  """List the contents of a leveldb database
+  """
+  if len(argv) == 1:
+    logging.error("Must specify name of database")
+
+  verbose = FLAGS.verbose
+
+  db = plyvel.DB(argv[1], create_if_missing=False)
+  for key, value in db:
+    k = int.from_bytes(key, "little", signed=False)
+    print(f"Key {k} value {value.decode()}")
+
+
+if __name__ == '__main__':
+  app.run(main)
