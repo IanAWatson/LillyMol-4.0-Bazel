f307b3e5abe968122caf7d1defb9ce74b1cad673
Ian Watson <ianiwatson@gmail.com>
2023-07-07 11:44:39 -0400

    Julia WIP

--- src/t.jl
+++ src/t.jl
@@ -13,8 +13,7 @@ module LillyMol
   export Molecule
   import Base: getindex, iterate, in
   getindex(m::Molecule, a::Int)=atom(m, a)
-  iterate(m::Molecule) = (m, 1)
-  iterate(m::Molecule, state) = (state == natoms(m) ? nothing : (m, state + 1))
+  iterate(m::Molecule, state=1) = (state >= natoms(m) ? nothing : (m[state], state + 1))
   iterate(a::Atom) = (a, 1)
   iterate(a::Atom, state) = (state == ncon(a) ? nothing : (a, state + 1))
   in(z::Int, m::Molecule) = (natoms(m, z) > 0)
@@ -44,13 +43,16 @@ println(LillyMol.greet(w))
 
 m = Molecule()
 build_from_smiles(m, "c1ccccc1OC foo")
+build_from_smiles(m, "CNOF foo")
 println("molecule has $(natoms(m)) atoms")
 print(LillyMol.smiles(m))
 println(LillyMol.name(m))
 
 for (index, atom) in enumerate(m)
+  println("$(index) is $(atom)")
   println("Atom $(index) is $(atomic_number(atom)) with $(ncon(atom)) connections, aromatic $(is_aromatic(m, index))")
 end
+
 for z in 1:8
   println("$(z) in $(z in m)")
 end
