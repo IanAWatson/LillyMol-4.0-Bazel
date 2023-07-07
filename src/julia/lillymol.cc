046e67d01476c0e0aa90b434ea93c7fad405ab6e
Ian Watson <ianiwatson@gmail.com>
2023-07-07 11:16:30 -0400

    Julia WIP

--- src/julia/lillymol.cc
+++ src/julia/lillymol.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 #include "jlcxx/jlcxx.hpp"
 
@@ -33,6 +34,32 @@ JLCXX_MODULE define_types_module(jlcxx::Module& types)
   ;
 }
 
+enum BondType {
+  kInvalidBond = 0,
+  kSingleBond = 1,
+  kDoubleBond = 2,
+  kTripleBond = 3,
+  kAromaticBond = 4
+};
+
+BondType
+ToBondType(const Bond& b) {
+  if (b.is_aromatic()) {
+    return kAromaticBond;
+  }
+  if (b.is_single_bond()) {
+    return kSingleBond;
+  }
+  if (b.is_double_bond()) {
+    return kDoubleBond;
+  }
+  if (b.is_triple_bond()) {
+    return kTripleBond;
+  }
+
+  return kInvalidBond;
+}
+
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
   mod.method("greet", &greet);
@@ -42,10 +69,144 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
     .method("set", &World::set)
     .method("greet", &World::greet)
   ;
+
+  mod.add_bits<BondType>("BondType", jlcxx::julia_type("CppEnum"));
+  mod.set_const("SINGLE", kSingleBond);
+  mod.set_const("DOUBLE", kDoubleBond);
+  mod.set_const("TRIPLE", kTripleBond);
+  mod.set_const("AROMATIC", kAromaticBond);
     
   mod.add_bits<FileType>("FileType", jlcxx::julia_type("CppEnum"));
   mod.set_const("SMI", FILE_TYPE_SMI);
   mod.set_const("SDF", FILE_TYPE_SDF);
+
+  mod.add_type<Set_of_Atoms>("Set_of_Atoms")
+    .constructor<>()
+    .method("contains", &Set_of_Atoms::contains)
+  ;
+    
+  mod.add_type<Bond>("Bond")
+    .method("a1", &Bond::a1)
+    .method("a2", &Bond::a2)
+    .method("btype",
+      [](const Bond& b)->BondType{
+        return ToBondType(b);
+      }
+    )
+    .method("is_single_bond",
+      [](const Bond& b)->bool{
+        return b.is_single_bond();
+      }
+    )
+    .method("is_double_bond",
+      [](const Bond& b)->bool{
+        return b.is_double_bond();
+      }
+    )
+    .method("is_triple_bond",
+      [](const Bond& b)->bool{
+        return b.is_triple_bond();
+      }
+    )
+    .method("is_aromatic",
+      [](const Bond& b)->bool{
+        return b.is_aromatic();
+      }
+    )
+    .method("is_aromatic_bond",
+      [](const Bond& b)->bool{
+        return b.is_aromatic();
+      }
+    )
+    .method("nrings",
+      [](const Bond& b) {
+        return b.nrings();
+      }
+    )
+    .method("bond_number", &Bond::bond_number)
+    .method("either_atom_set_in_array", &Bond::either_atom_set_in_array)
+    .method("atoms",
+      [](const Bond& b)->std::tuple<atom_number_t, atom_number_t>{
+        return std::make_tuple(b.a1(), b.a2());
+      }
+    )
+    .method("involves",
+      [](const Bond& a, atom_number_t o)->bool{
+        return a.involves(o);
+      }
+    )
+    .method("involves",
+      [](const Bond& a, atom_number_t o1, atom_number_t o2)->bool{
+        return a.involves(o1, o2);
+      }
+    )
+    .method("joins",
+      [](const Bond& b1, const Bond& b2)->bool{
+        return b1.joins(&b2);
+      }
+    )
+  ;
+
+  mod.add_type<Atom>("Atom")
+    // .constructor<jlcxx::cxxint_t>(true) // with finalizer
+    .method("valence_ok", &Atom::valence_ok)
+    .method("ncon",
+      [](const Atom& a) {
+        return a.ncon();
+      }
+    )
+    .method("isotope", &Atom::isotope)
+    .method("lone_pair_count",
+      [](Atom&a){
+        int lp;
+        if (a.lone_pair_count(lp)) {
+          return lp;
+        }
+        return 0;
+      }
+    )
+    .method("nbonds",
+      [](const Atom& a) {
+        return a.nbonds();
+      }
+    )
+    .method("nbonds",
+      [](Atom& a) {
+        return a.nbonds();
+      }
+    )
+    .method("bond_to_atom",
+      [](const Atom& a, atom_number_t o)->const Bond*{
+        return a.bond_to_atom(o);
+      }
+    )
+    .method("is_bonded_to",
+      [](const Atom& a, atom_number_t o)->bool{
+        return a.is_bonded_to(o);
+      }
+    )
+    .method("is_halogen", &Atom::is_halogen)
+    .method("atomic_number", &Atom::atomic_number)
+    .method("atomic_symbol",
+      [](const Atom& a)->std::string{
+        return a.atomic_symbol().AsString();
+      }
+    )
+    .method("implicit_hydrogens", &Atom::implicit_hydrogens)
+    .method("formal_charge", &Atom::formal_charge)
+    .method("other", &Atom::other)
+    .method("connections",
+      [](const Atom& a, atom_number_t me)->Set_of_Atoms{
+        return a.connections(me);
+      }
+    )
+    .method("atomic_weight", &Atom::atomic_weight)
+    .method("saturated", 
+      [](const Atom& a)->bool{
+        return a.fully_saturated();
+      }
+    )
+  ;
 
   mod.add_type<Molecule>("Molecule")
     .constructor<>()
@@ -65,13 +226,6 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
         return m.name().AsString();
       }
     )
-
-    .method("build_from_smiles", static_cast<int (Molecule::*)(const std::string& smiles)>(&Molecule::build_from_smiles))
-    .method("smiles", 
-      [](Molecule& m)->std::string{
-        return m.smiles().AsString();
-      }
-    )
     .method("natoms",
       [](const Molecule& m)->int{
         return m.natoms();
@@ -87,6 +241,35 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
         return m.natoms(asymbol.c_str());
       }
     )
+    .method("nrings",
+      [](Molecule& m) {
+        return m.nrings();
+      }
+    )
+    .method("nrings",
+      [](Molecule& m, atom_number_t a) {
+        return m.nrings(a);
+      }
+    )
+
+    .method("build_from_smiles", static_cast<int (Molecule::*)(const std::string& smiles)>(&Molecule::build_from_smiles))
+    .method("smiles", 
+      [](Molecule& m)->std::string{
+        return m.smiles().AsString();
+      }
+    )
+    .method("unique_smiles", 
+      [](Molecule& m)->std::string{
+        return m.unique_smiles().AsString();
+      }
+    )
+    .method("random_smiles", 
+      [](Molecule& m)->std::string{
+        return m.random_smiles().AsString();
+      }
+    )
+    .method("is_aromatic", &Molecule::is_aromatic)
+    .method("atom", &Molecule::atom)
   ;
 }
 
---
+++ src/t.jl
@@ -0,0 +1,56 @@
+module LillyMol
+  using CxxWrap
+  @wrapmodule(joinpath("bazel-bin/julia/","lillymol_julia.so"))
+
+  function __init__()
+    @initcxx
+  end
+  export World
+
+  export FileType
+  export BondType
+  export greet
+  export Molecule
+  import Base: getindex, iterate, in
+  getindex(m::Molecule, a::Int)=atom(m, a)
+  iterate(m::Molecule) = (m, 1)
+  iterate(m::Molecule, state) = (state == natoms(m) ? nothing : (m, state + 1))
+  iterate(a::Atom) = (a, 1)
+  iterate(a::Atom, state) = (state == ncon(a) ? nothing : (a, state + 1))
+  in(z::Int, m::Molecule) = (natoms(m, z) > 0)
+  in(atom::Int, a::Atom) = involves(a, atom)
+  length(m::Molecule) = natoms(m)
+  size(m::Molecule) = natoms(m)
+  export getindex
+  export iterate
+  export length
+  export in
+  export natoms, smiles, unique_smiles, nrings, build_from_smiles, is_aromatic
+  export atomic_number
+  export ncon, nbonds, involves
+  export atomic_number
+end
+#getindex(m::LillyMol.Molecule, a::Int64)=LillyMol.atom(m, a)
+
+using .LillyMol
+
+# Call greet and show the result
+@show LillyMol.greet()
+
+w = LillyMol.World()
+println(LillyMol.greet(w));
+LillyMol.set(w, "hello")
+println(LillyMol.greet(w))
+
+m = Molecule()
+build_from_smiles(m, "c1ccccc1OC foo")
+println("molecule has $(natoms(m)) atoms")
+print(LillyMol.smiles(m))
+println(LillyMol.name(m))
+
+for (index, atom) in enumerate(m)
+  println("Atom $(index) is $(atomic_number(atom)) with $(ncon(atom)) connections, aromatic $(is_aromatic(m, index))")
+end
+for z in 1:8
+  println("$(z) in $(z in m)")
+end
